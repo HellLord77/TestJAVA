3a934e2c613608461a0f37f52aa501eb534c0573
HellLord <ratul.debnath.year@gmail.com>
2024-07-09 23:48:02 +0600

    [~] 2.8

--- src/chapter2/exercise8/Main.java
+++ src/chapter2/exercise8/Main.java
@@ -2,7 +2,6 @@ package chapter2.exercise8;
 
 import java.time.OffsetTime;
 import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
@@ -14,7 +13,6 @@ public class Main {
 
         ZoneOffset zoneOffset = ZoneOffset.ofHours(offset);
         OffsetTime current = OffsetTime.now(zoneOffset);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-        System.out.println("The current time is " + current.format(formatter));
+        System.out.println("The current time is " + current.getHour() + ":" + current.getMinute() + ":" + current.getSecond());
     }
 }
