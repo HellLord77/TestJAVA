341aca5cc619006d43e58a50caa383374400b6c9
HellLord <ratul.debnath.year@gmail.com>
2024-07-11 23:34:09 +0600

    [~] 3.4

--- src/chapter3/exercise4/Main.java
+++ src/chapter3/exercise4/Main.java
@@ -2,7 +2,7 @@ package chapter3.exercise4;
 
 public class Main {
     public static void main(String[] args) {
-        String[] months = new String[]{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
         int month = (int) (Math.random() * 12) + 1;
         System.out.println("Month " + month + ": " + months[month - 1]);
