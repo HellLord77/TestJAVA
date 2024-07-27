0915847bb8d7b62589007686831af0b2e1272139
HellLord <ratul.debnath.year@gmail.com>
2024-07-27 23:12:26 +0600

    [~] 5.51

--- src/chapter5/exercise51/Main.java
+++ src/chapter5/exercise51/Main.java
@@ -1,25 +1,27 @@
 package chapter5.exercise51;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static String getLongestCommonPrefix(String s1, String s2) {
-        String longest = "";
-        for (int i = 0; i < s1.length(); ++i) {
-            for (int j = i + 1; j < s1.length(); ++j) {
-                if (j - i > longest.length()) {
-                    for (int k = 0; k < s2.length(); ++k) {
-                        for (int l = k + 1; l < s2.length(); ++l) {
-                            if (s1.substring(i, j).compareTo(s2.substring(k, l)) == 0) {
-                                longest = s1.substring(i, j);
-                                break;
-                            }
-                        }
+    public static char[] getLongestCommonPrefix(char[] s1, char[] s2) {
+        char[] result = new char[0];
+        int[][] lengths = new int[s1.length + 1][s2.length + 1];
+        for (int i = 0; i <= s1.length; i++) {
+            for (int j = 0; j <= s2.length; j++) {
+                if (i == 0 || j == 0) {
+                    lengths[i][j] = 0;
+                } else if (s1[i - 1] == s2[j - 1]) {
+                    lengths[i][j] = lengths[i - 1][j - 1] + 1;
+                    if (lengths[i][j] > result.length) {
+                        result = Arrays.copyOfRange(s1, j - lengths[i][j], j);
                     }
+                } else {
+                    lengths[i][j] = 0;
                 }
             }
         }
-        return longest;
+        return result;
     }
 
     public static void main(String[] args) {
@@ -31,7 +33,7 @@ public class Main {
         System.out.print("Enter the second string: ");
         String second = scanner.nextLine();
 
-        String prefix = getLongestCommonPrefix(first, second);
+        String prefix = String.valueOf(getLongestCommonPrefix(first.toCharArray(), second.toCharArray()));
         if (prefix.isEmpty()) {
             System.out.printf("%s and %s have no common prefix%n", first, second);
         } else {
